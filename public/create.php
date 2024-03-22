5f9f013d2bb337af6b15a5b396421c80379e6a3d
idp-tk <149179396+idp-tk@users.noreply.github.com>
2024-03-22 15:43:46 +0000

    Update create.php

--- public/create.php
+++ public/create.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'lhcp4028.webapps.net';
-$DATABASE_USER = 'h75tbc3z_idpadmin';
-$DATABASE_PASS = 'terminal998';
-$DATABASE_NAME = 'h75tbc3z_itemdatabase';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+include 'dbconnect.php';
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password'], $_POST['email'])) {
 	// Could not get the data that should have been sent.
