250235ca3b39444ad22690eddb5b46aebe688676
idp-tk <149179396+idp-tk@users.noreply.github.com>
2024-03-22 15:48:29 +0000

    Update templates.php

--- public/templates.php
+++ public/templates.php
@@ -9,7 +9,7 @@ function pdo_connect_mysql() {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
+    	exit('Failed to connect to MySQL: ' . $exception);
     }
 }
 
