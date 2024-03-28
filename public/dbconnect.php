a0bbfe2d8304df9f516d98676905977203336b82
idp-tk <149179396+idp-tk@users.noreply.github.com>
2024-03-28 01:42:33 +0000

    Update dbconnect.php

--- public/dbconnect.php
+++ public/dbconnect.php
@@ -3,8 +3,11 @@ $DATABASE_HOST = 'idp-database.mysql.database.azure.com';
 $DATABASE_USER = 'idpadmin';
 $DATABASE_PASS = 'password112!';
 $DATABASE_NAME = 'itemdatabase';
+$options = array(
+	PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem'
+);
 // Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME, $options);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
