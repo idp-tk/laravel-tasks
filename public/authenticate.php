ebb1cf3fd7c647df16d05b7b00a5a49fb852e879
idp-tk <149179396+idp-tk@users.noreply.github.com>
2024-03-28 01:49:49 +0000

    Modified PDO Connection

--- public/authenticate.php
+++ public/authenticate.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'dbconnect.php';
+//include 'dbconnect.php';
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if (!isset($_POST['username'], $_POST['password'])) {
@@ -8,7 +8,7 @@ if (!isset($_POST['username'], $_POST['password'])) {
 	exit('Please fill both the username and password fields!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $pdo->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
