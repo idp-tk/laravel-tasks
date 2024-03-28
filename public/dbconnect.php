1faaeeab90ba8323277c827bcf06cbc5cfb7a664
idp-tk <149179396+idp-tk@users.noreply.github.com>
2024-03-28 02:38:20 +0000

    Update dbconnect.php

--- public/dbconnect.php
+++ public/dbconnect.php
@@ -5,8 +5,8 @@ $DATABASE_PASS = 'password112!';
 $DATABASE_NAME = 'itemdatabase';
 
 // Try and connect using the info above.
-$mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-$mysqli->ssl_set(NULL, NULL, 'DigiCertGlobalRootCA.crt.pem', NULL, NULL);
+$con->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$con->ssl_set(NULL, NULL, 'DigiCertGlobalRootCA.crt.pem', NULL, NULL);
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
